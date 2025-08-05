4e73572583bd5cfd3b25c8251409c40dd033a071
Sam Bird <sam@sambird.dev>
2025-08-05 15:54:29 -0500

    report query time

--- query_stream.py
+++ query_stream.py
@@ -50,6 +50,9 @@ class QueryStream:
         self._run_query_set()
         self._run_refresh_function_2(0)
 
+        for i, time in self.query_times:
+            logging.debug(f'Power:Q{i + 1} : {round(time, 2)}s')
+
         self._finalise()
     
     def run_throughput(self):
